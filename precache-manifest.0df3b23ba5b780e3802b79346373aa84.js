b4b37e27e04815fe8b492efddd325b4c8fe0fcb0
mozgbrasil <mozgbrasil@gmail.com>
2021-08-31 23:18:06 +0000

    Updates

---
+++ precache-manifest.0df3b23ba5b780e3802b79346373aa84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e54249e17ce6fb01f17",
+    "url": "/react-rekit/static/css/main.a20d45cd.chunk.css"
+  },
+  {
+    "revision": "0e54249e17ce6fb01f17",
+    "url": "/react-rekit/static/js/main.0e54249e.chunk.js"
+  },
+  {
+    "revision": "0cadd9b4f81a38f55cac",
+    "url": "/react-rekit/static/js/1.0cadd9b4.chunk.js"
+  },
+  {
+    "revision": "732d7f8b54a7fbe3527e",
+    "url": "/react-rekit/static/js/runtime~main.732d7f8b.js"
+  },
+  {
+    "revision": "5c11e8dba5b3d2dfee139094345c46c1",
+    "url": "/react-rekit/static/media/rekit-react.5c11e8db.png"
+  },
+  {
+    "revision": "30cec3dc7d0fe454367d8d317e106d35",
+    "url": "/react-rekit/index.html"
+  }
+];
